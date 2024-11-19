882c858c42ae56a074d6f43da09911b0cf457009
Kapyrin Illia <rock12345133@gmail.com>
2024-11-19 18:59:18 +0200

    task-3

---
+++ task-3/script3.1.js
@@ -0,0 +1,13 @@
+const string = "String";
+const number = 1;
+const boolean = true;
+let myUndefined;
+const symbol = Symbol("example");
+const bigInt = 1n;
+const object = Object();
+const message = `JS types:\n\n${typeof string}\n${typeof number}\n${typeof boolean}\n${null}\n${typeof myUndefined}\n${typeof symbol}\n${typeof bigInt}\n${typeof object}`;
+console.log(message);
+
+
+
+
---
+++ task-3/script3.2.js
@@ -0,0 +1,7 @@
+const message1 = prompt("Enter first message", "");
+const message2 = prompt("Enter second message", "");
+const message3 = prompt("Enter third message","");
+
+alert(
+  `First message: ${message1}\nSecond Message: ${message2}\nThird message: ${message3}`,
+);
