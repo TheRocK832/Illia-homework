5e14e35ea4ea51f2a1c30323504ce87c536afd40
Kapyrin Illia <rock12345133@gmail.com>
2024-11-25 14:50:19 +0200

    hw 4-3

--- task-4/script4-3.js
+++ task-4/script4-3.js
@@ -1,6 +1,31 @@
 const yearOfBirth = prompt("Введіть ваш рік народження");
 const city = prompt("Введіть ваше місто");
 const favoriteSport = prompt("Введіть ваш улюблений вид спорту");
-console.log(yearOfBirth);
-console.log(city);
-console.log(favoriteSport);
+// console.log(yearOfBirth);
+// console.log(city);
+// console.log(favoriteSport);
+if (yearOfBirth && city && favoriteSport) {
+    const todayDate = new Date().getFullYear();
+    const age = todayDate - parseInt(yearOfBirth);
+
+    
+    let cityMessage;
+    switch (city.toLowerCase()) {
+        case "київ":
+            cityMessage = "столиці України.";
+            break;
+        case "вашингтон":
+            cityMessage = "столиці США.";
+            break;
+        case "лондон":
+                cityMessage = "столиці Великобританії.";
+                break;
+        default: 
+            cityMessage = `місті ${city}.`;
+    }
+
+    alert(`Тобі ${age} років. \nТи живеш в ${cityMessage} \nУлюблений вид спорту ${favoriteSport}`);
+} else {
+    alert("Ви не заповнили дані. Спробуйте ще раз!");
+}
+
