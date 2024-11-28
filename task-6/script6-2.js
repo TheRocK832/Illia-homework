a8803cf73dd950124128e82685073c3ca6d44e3b
Kapyrin Illia <rock12345133@gmail.com>
2024-11-28 16:30:21 +0200

    hw6-2

---
+++ task-6/script6-2.js
@@ -0,0 +1,19 @@
+
+const array = [{}, 1, 2, 3, 4, 5, 6, 'string', null];
+
+function foo(array) {
+    let total = 0; 
+    let count = 0; 
+
+    for (let item of array) {
+        if (typeof item === 'number') { 
+            total = total + item;
+            count++;
+        }
+    }
+    return total / count; 
+}
+
+const result = foo(array);
+
+alert(result);
